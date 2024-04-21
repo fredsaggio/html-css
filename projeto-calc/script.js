89cc64957b013d240e821d3f5ce0eb106a4a0e7d
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-04-20 23:01:13 -0300

    começando com javascript

--- projeto-calc/script.js
+++ projeto-calc/script.js
@@ -1,8 +1,13 @@
-function allScope() {
+const previousOperationText = document.getElementById('previous-operation')
+const currentOperationText = document.getElementById('current-operation')
+const buttons = document.querySelectorAll()
 
+class Calculator{}
 
+buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const value = e.target.innerText;
 
+    })
 
-
-    
-}
+})
